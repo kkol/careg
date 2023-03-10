840f986137f290929aed45a57985736b7b44e9f0
kolasak <kolasa.konrad92@gmail.com>
2023-03-10 15:55:11 +0100

    initial commit with Srping Boot confiuration

---
+++ src/main/java/kkol/crc/careg/CaregApplication.java
@@ -0,0 +1,13 @@
+package kkol.crc.careg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaregApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaregApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kkol/crc/careg/CaregApplicationTests.java
@@ -0,0 +1,13 @@
+package kkol.crc.careg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaregApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
